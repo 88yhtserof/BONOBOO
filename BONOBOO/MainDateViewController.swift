bc6aa79f92160ccdfbe6c481026648673b350b5d
LimYunhwi <88yhtserof@naver.com>
2021-11-07 23:18:00 +0900

    #2 main feat : 날짜 설정 버튼 tap 이벤트 시 화면 이동 그리고 DatePicker 배치

---
+++ BONOBOO/MainDateViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainDateViewController.swift
+//  BONOBOO
+//
+//  Created by limyunhwi on 2021/11/07.
+//
+
+import UIKit
+
+class MainDateViewController: UIViewController {
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        datePicker.backgroundColor = .white
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
