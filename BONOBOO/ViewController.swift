e647ce77fa9bb73875dd1d1988a2d6ecea622de4
LimYunhwi <88yhtserof@naver.com>
2021-11-08 23:31:01 +0900

    #2 main feat : 이미지 클릭 시 상세 페이지 이동(1)
    1. 이미지 클릭 가능하도록 구현

--- BONOBOO/ViewController.swift
+++ BONOBOO/ViewController.swift
@@ -9,10 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var imgPoster: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tapImageView()
     }
     
+    func tapImageView() {
+        let recognizerImage = UITapGestureRecognizer(target: self, action: #selector(tapActionImageView))
+        imgPoster.isUserInteractionEnabled = true
+        imgPoster.addGestureRecognizer(recognizerImage)
+        
+    }
+    
+    @objc func tapActionImageView(sender: UITapGestureRecognizer) {
+        print("Tap")
+    }
 }
 
