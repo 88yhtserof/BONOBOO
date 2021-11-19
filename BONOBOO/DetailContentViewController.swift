b7b73539bf4538b89b6a177017a19dc5e7caacc4
LimYunhwi <88yhtserof@naver.com>
2021-11-20 01:15:57 +0900

    #2 main feat : 이미지 클릭 시 상세 페이지 이동(3)
    1. 메인뷰에 NavigationController 설정하기
    2. tapActionImageView함수 코드 정리하기
    3. 메인 뷰에서 Navigation Top bar 숨기기
    4. 상세뷰에서 Navigation Top bar 보이기

--- BONOBOO/DetailContentViewController.swift
+++ BONOBOO/DetailContentViewController.swift
@@ -12,18 +12,11 @@ class DetailContentViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setNavigationBar()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setNavigationBar() {
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
     }
-    */
-
+    
 }
--- BONOBOO/ViewController.swift
+++ BONOBOO/ViewController.swift
@@ -17,6 +17,10 @@ class ViewController: UIViewController {
         tapImageView()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        setNavigationBar()
+    }
+    
     func tapImageView() {
         let recognizerImage = UITapGestureRecognizer(target: self, action: #selector(tapActionImageView))
         imgPoster.isUserInteractionEnabled = true
@@ -25,15 +29,13 @@ class ViewController: UIViewController {
     }
     
     @objc func tapActionImageView(sender: UITapGestureRecognizer) {
-        print("tap1")
-        //let storyboard = UIStoryboard(name: "Main", bundle: nil)
         guard let viewController = self.storyboard?.instantiateViewController(identifier: "DetailContentViewController") else{return}
-        print(self)
-        //self가 NavigationViewController가 아니라서 navigationVContoller 동작 안 한다.
         
-        //self.present(viewController, animated: true, completion: nil)
         self.navigationController?.pushViewController(viewController, animated: true)
-        print("tap2")
+    }
+    
+    func setNavigationBar() {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
     }
 }
 
