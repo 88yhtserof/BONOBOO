aa32ceaef5d373580a6354b281ba1f13c7a5eabf
LimYunhwi <88yhtserof@naver.com>
2021-11-23 16:43:15 +0900

    #2 main feat : 사용자 test 파일 수정
    1. Movie 구조체 생성
    2. User 구조체 수정
    3. Movie타입의 배열 생성

--- BONOBOO/test.swift
+++ BONOBOO/test.swift
@@ -8,12 +8,17 @@
 import Foundation
 
 struct User {
-    var posters: [String]
+    var movies: [Movie]
     var star: Int
 }
+let user1: User = User(movies: movies, star: movies.count)
 
-var posters: [String] = ["LittleForest.jpeg",
-                         "Howl'sMovingCastle.jpeg",
-                         "HarryPoterAndTheSorcerer'sStone.jpeg"]
+struct Movie {
+    var name: String
+    var date: String
+    var poster: String
+}
 
-let user1: User = User(posters: posters, star: posters.count)
+var movies: [Movie] = [Movie(name: "리틀포레스트", date: "2021.11.01", poster: "LittleForest.jpeg"),
+                      Movie(name: "하울의 움직이는 성", date: "2021.10.01", poster: "Howl'sMovingCastle.jpeg"),
+                      Movie(name: "해리포터와 마법사의 돌", date: "2021.9.01", poster: "HarryPoterAndTheSorcerer'sStone.jpeg")]
